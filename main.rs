9440dfcc36a788c6053234b2ce2456bbbbc5ecee
Hackerr7767 <97411928+Hackerr7767@users.noreply.github.com>
2022-04-21 01:39:17 +0200

    UPDATE: Added Button Struct

--- main.rs
+++ main.rs
@@ -3,10 +3,18 @@ struct TextView {
     color: String,
     font: String
 }
+struct Button {
+button_value: String,
+    button_color: String,
+}
 fn main() {
     let text = TextView {
         value: String::from("Hello"),
         color: String::from("red"),
         font: String::from("Arial")
+    };
+    let my_btn = Button {
+    button_value: String::from("Click this"),
+        button_color: String::from("red")
     };
 }
