f339889c88b500bd390d66ab8d9cd28e14152887
Hackerr7767 <97411928+Hackerr7767@users.noreply.github.com>
2022-04-20 21:23:10 +0200

    Hello World React Native App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+export default class App extends React.Component {
+return(
+  <View>
+   <Text>Hello World!</Text>
+   </View>
+  )
+  const styles = StyleSheet.create({
+  View: {
+  textAlign: 'center'
+  }
+  })
+}
